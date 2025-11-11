da510023fd294643bd54428b43572457110e2a92
gabrijel0741 <gabrijel0741@gmail.com>
2025-11-11 13:29:27 +0100

    Changes to main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Home, User } from "lucide-react";
+import { CloudOff, Home, LayoutDashboard, Search} from "lucide-react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass';
 import { IzdvojeniSkupoviPodataka } from './components/IzdvojeniSkupoviPodataka';
@@ -31,29 +31,35 @@ function App() {
 
   return (
     <>
+      <div className="home-profile-selector">
+        <button
+          className={`selector-btn ${selected === "home" ? "active-home" : ""}`}
+          onClick={() => setSelected("home")}
+        >
+          <Home size={24} />
+        </button>
+
+        <button
+          className={`selector-btn profile-btn ${selected === "profile" ? "active-profile" : ""}`}
+          onClick={() => setSelected("profile")}
+        >
+          <LayoutDashboard  size={24} />
+        </button>
+      </div>
       <div className="main-container">
-        <div className='search-home-profile-div'>
+        <div className='search-skupovi-div'>
+          <div className='ikona-naslov-div search-skupovi-div'>
+                <Search className="ikona"/>
+                <h2 className='search-skupovi-h2'>Pretražite skupove podataka</h2>
+          </div>
           <form className="search">
-              <input type="text" className = "search-input"/>
+              <input type="text" placeholder="Unesite naziv skupa podataka" className = "search-input"/>
               <button className="search-button" type = "submit" onClick ={filterAnalyses}><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
           </form>
-          <div className="home-profile-selector">
-            <button
-              className={`selector-btn ${selected === "home" ? "active-home" : ""}`}
-              onClick={() => setSelected("home")}
-            >
-              <Home size={24} />
-            </button>
-
-            <button
-              className={`selector-btn profile-btn ${selected === "profile" ? "active-profile" : ""}`}
-              onClick={() => setSelected("profile")}
-            >
-              <User size={24} />
-            </button>
-          </div>
         </div>
-        <IzdvojeniSkupoviPodataka />
+        <div>
+          <IzdvojeniSkupoviPodataka />
+        </div>
       </div>
     </>
   );
