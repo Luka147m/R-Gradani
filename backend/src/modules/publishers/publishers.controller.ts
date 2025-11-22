2cc7baa60bdcca850cb159711d4462c5ea881caf
Toma Žulj <tomazulj10@gmail.com>
2025-11-22 12:43:26 +0100

    fix: updated responses in controllers

--- backend/src/modules/publishers/publishers.controller.ts
+++ backend/src/modules/publishers/publishers.controller.ts
@@ -1,13 +1,11 @@
-import { Request, Response } from 'express';
-import * as publishersService from './publishers.service';
-import { sendSuccess, sendError } from '../../utils/response';
+import { Request, Response } from "express";
+import * as publishersService from "./publishers.service";
 
 export const getPublishers = async (req: Request, res: Response) => {
-
-    try {
-        const publishers = await publishersService.fetchPublishers();
-        sendSuccess(res, 200, 'Comments fetched successfully.', publishers);
-    } catch (error) {
-        sendError(res, 500, `${error}`);
-    }
+  try {
+    const publishers = await publishersService.fetchPublishers();
+    res.status(200).json(publishers);
+  } catch (error) {
+    res.status(500).json({ message: `${error}` });
+  }
 };
