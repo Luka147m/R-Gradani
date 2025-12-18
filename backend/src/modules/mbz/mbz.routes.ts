f91d492fc8da97d1985850ef687dd877ccc80d3d
Luka Miličević <luka.milicevic147@gmail.com>
2025-12-18 15:19:56 +0100

    file import feature openapi

--- backend/src/modules/mbz/mbz.routes.ts
+++ backend/src/modules/mbz/mbz.routes.ts
@@ -31,11 +31,11 @@ const handleMulterError = (err: any, req: Request, res: Response, next: NextFunc
 
 // Disableano dok ne bude dodana nekakva zastita na ovu rutu
 
-// mbzRouter.post(
-//     '/',
-//     upload.single('file'),
-//     handleMulterError,
-//     uploadMbz
-// );
+mbzRouter.post(
+    '/',
+    upload.single('file'),
+    handleMulterError,
+    uploadMbz
+);
 
 export default mbzRouter;
