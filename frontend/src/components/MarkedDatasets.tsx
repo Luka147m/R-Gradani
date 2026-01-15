f2337529915c19f779b1a77f6fbb55f76446edbc
Jurica-Laljak <jurica.laljak@gmail.com>
2026-01-15 08:51:05 +0100

    fix: upload button style

--- frontend/src/components/MarkedDatasets.tsx
+++ frontend/src/components/MarkedDatasets.tsx
@@ -1,89 +1,106 @@
-import { DatasetCard} from './DatasetCard.tsx';
-import { useState, useEffect } from 'react';
-import '../style/IzdvojeniSkupoviPodataka.css';
-import { Bookmark } from 'lucide-react';
-import '../style/HomePage.css'
-import api from '../api/axios.tsx'
-import type { getDatasetDTO } from '../DTOs/getDatasetDTO.ts';
-import ApiButton from './ApiButton.tsx';
+import { DatasetCard } from "./DatasetCard.tsx";
+import { useState, useEffect } from "react";
+import "../style/IzdvojeniSkupoviPodataka.css";
+import { Bookmark, CloudUpload } from "lucide-react";
+import "../style/HomePage.css";
+import api from "../api/axios.tsx";
+import type { getDatasetDTO } from "../DTOs/getDatasetDTO.ts";
+import ApiButton from "./ApiButton.tsx";
+import IconText from "./IconText.tsx";
 
 export const MarkedDatasets = () => {
-    const [markedDatasets, setMarkedDatasets] = useState<getDatasetDTO[]>([]);
-    const [importIsSelected, setImportIsSelected] = useState<boolean>(false);
-    const [file, setFile] = useState<File | null>(null);
+  const [markedDatasets, setMarkedDatasets] = useState<getDatasetDTO[]>([]);
+  const [importIsSelected, setImportIsSelected] = useState<boolean>(false);
+  const [file, setFile] = useState<File | null>(null);
 
-    useEffect(() => {
-        const fetchMarkedDatasets = async () => {
-            const arr = JSON.parse(localStorage.getItem('savedDatasets') || '[]');
-            if (arr.length === 0) return;
-            const response = await api.post('/skupovi/ids', { ids: arr });
-            setMarkedDatasets(response.data);
-        };
-        fetchMarkedDatasets();
-    }, []);
+  useEffect(() => {
+    const fetchMarkedDatasets = async () => {
+      const arr = JSON.parse(localStorage.getItem("savedDatasets") || "[]");
+      if (arr.length === 0) return;
+      const response = await api.post("/skupovi/ids", { ids: arr });
+      setMarkedDatasets(response.data);
+    };
+    fetchMarkedDatasets();
+  }, []);
 
-    const importComments = () => {
-        setImportIsSelected(true);
+  const importComments = () => {
+    setImportIsSelected(true);
+  };
+
+  const checkFileType = (fileInt: File | null) => {
+    if (fileInt && !fileInt.name.endsWith(".mbz")) {
+      alert("Datoteka mora biti u .mbz formatu.");
+      setFile(null);
+      return;
+    }
+    setFile(fileInt);
+  };
+
+  const submitImport = async () => {
+    if (!file) {
+      alert("Nije odabran niti jedan file.");
+      return;
     }
 
-    const checkFileType = (fileInt: File | null) => {
-        if(fileInt && !fileInt.name.endsWith('.mbz')){
-            alert("Datoteka mora biti u .mbz formatu.");
-            setFile(null);
-            return;
-        }
-        setFile(fileInt);
-    }
+    await api.post("/upload", file, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
 
-    const submitImport = async () => {
-        if(!file) {
-            alert("Nije odabran niti jedan file.");
-            return;
-        }
+    setImportIsSelected(false);
+    setFile(null);
+  };
 
-        await api.post('/upload', file, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-            },
-        });
+  return (
+    <>
+      <div className="search-skupovi-div">
+        <div className="ikona-naslov-div">
+          <Bookmark className="ikona" />
+          <h1 className="search-skupovi-h1">Zabilježeni skupovi podataka</h1>
+        </div>
+        {markedDatasets.length > 0 ? (
+          <div className="skupovi-podataka-grid marked-datasets">
+            {markedDatasets.map((skupPodataka) => (
+              <DatasetCard key={skupPodataka.id} {...skupPodataka} />
+            ))}
+          </div>
+        ) : (
+          <div>Nema zabilježenih skupova podataka.</div>
+        )}
+        <div className="button-wrapper">
+          <button onClick={importComments} className="api-button import-button">
+            <IconText
+              icon={CloudUpload}
+              text="Objavi analize skupova podataka"
+            ></IconText>
+          </button>
+        </div>
+      </div>
 
-        setImportIsSelected(false);
-        setFile(null);
-    }
-
-    return (
-        <>
-            <div className = "search-skupovi-div">
-                <div className='ikona-naslov-div'>
-                    <Bookmark className='ikona' />
-                    <h1 className='search-skupovi-h1'>Zabilježeni skupovi podataka</h1>
-                </div>
-                <button onClick={importComments} className="api-button import-button">Uvezi</button>
-                <div className="skupovi-podataka-grid marked-datasets">
-                    {markedDatasets.map((skupPodataka) => (
-                        <DatasetCard
-                                key={skupPodataka.id}
-                                {...skupPodataka}
-                            />
-                    ))}
-                </div>
-            </div>
-            {importIsSelected && (
-                <div className="overlay">
-                    <div className="import-modal">
-                        <h2>Uvoz zabilježenih skupova podataka</h2>
-                        <button className="import-modal-close-button" onClick={() => setImportIsSelected(false)}>x</button>
-                        <input 
-                            type="file" 
-                            accept=".mbz"
-                            onChange={(e) => checkFileType(e.target.files ? e.target.files[0] : null)} 
-                        />
-                        <ApiButton apiCall={submitImport} className="api-button">
-                            Potvrdi
-                        </ApiButton>
-                    </div>
-                </div>
-            )}
-        </>
-    );
+      {importIsSelected && (
+        <div className="overlay">
+          <div className="import-modal">
+            <h2>Uvoz zabilježenih skupova podataka</h2>
+            <button
+              className="import-modal-close-button"
+              onClick={() => setImportIsSelected(false)}
+            >
+              x
+            </button>
+            <input
+              type="file"
+              accept=".mbz"
+              onChange={(e) =>
+                checkFileType(e.target.files ? e.target.files[0] : null)
+              }
+            />
+            <ApiButton apiCall={submitImport} className="api-button">
+              Potvrdi
+            </ApiButton>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
