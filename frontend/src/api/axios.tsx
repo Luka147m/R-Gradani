7915f34240fdb1972be6ead0ef7310c6adadd297
Toma Žulj <tomazulj10@gmail.com>
2026-01-22 08:45:39 +0100

    fix: axios fix

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "/api",
   timeout: 10000,
 });
 
@@ -10,7 +10,7 @@ api.interceptors.response.use(
   (error) => {
     console.error("API Error:", error.response?.data || error.message);
     return Promise.reject(error);
-  }
+  },
 );
 
 export default api;
