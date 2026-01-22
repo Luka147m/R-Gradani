a4530826b83d97b093d011652d10c392c809e6ca
Jurica-Laljak <jurica.laljak@gmail.com>
2026-01-22 08:58:10 +0100

    style: color changes and header align

--- frontend/src/style/global/colors.ts
+++ frontend/src/style/global/colors.ts
@@ -1,12 +1,12 @@
 export const colors = {
   background: "#121212",
   surface: "#1e1e1e",
-  primary: "#9540fc",
-  secondary: "#03dac6",
+  primary: "#811BFE",
+  secondary: "#0efbe4",
   "text-light": "#ffffff",
   "text-dark": "#272727",
-  error: "#d62828",
+  error: "#c22525",
   warning: "#ffb703",
-  correct: "#2a9d8f",
+  correct: "#2a9d5c",
   stats: "#ff8042",
 };
